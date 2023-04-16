1a16e92ee9b8ecf9bdbf6a43a6a2e0ff35d9ed12
Rafinha19 <rafael.rivero@udc.es>
2023-04-16 19:11:25 +0200

    Cambio el tamaño del carreteDetail.dart y login en singlechill scroll

--- lib/loginView.dart
+++ lib/loginView.dart
@@ -42,147 +42,149 @@ class _LoginViewState extends State<LoginView> {
       onWillPop: () async => false,
       child: Scaffold(
         body: Center(
-            child: Padding(
+            child: SingleChildScrollView(
+              child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 15.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Text(
-                  AppLocalizations.of(context)!.memento,
-                  textAlign: TextAlign.center,
-                  textScaleFactor: 4.0,
-                  overflow: TextOverflow.ellipsis,
-                  style: const TextStyle(fontWeight: FontWeight.bold),
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    AppLocalizations.of(context)!.memento,
+                    textAlign: TextAlign.center,
+                    textScaleFactor: 4.0,
+                    overflow: TextOverflow.ellipsis,
+                    style: const TextStyle(fontWeight: FontWeight.bold),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: TextField(
-                  controller: usernameController,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: AppLocalizations.of(context)!.username,
-                      hintText: AppLocalizations.of(context)!.username_hint),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    controller: usernameController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: AppLocalizations.of(context)!.username,
+                        hintText: AppLocalizations.of(context)!.username_hint),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: TextField(
-                  obscureText: true,
-                  controller: passwordController,
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: AppLocalizations.of(context)!.password,
-                      hintText: AppLocalizations.of(context)!.password_hint),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: TextField(
+                    obscureText: true,
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: AppLocalizations.of(context)!.password,
+                        hintText: AppLocalizations.of(context)!.password_hint),
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: Container(
-                  height: 50,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      color: Colors.orange,
-                      borderRadius: BorderRadius.circular(10)),
-                  child: TextButton(
-                    onPressed: () async {
-                      LoadingOverlay.of(context).show();
-                      var username = usernameController.text.trim();
-                      var password = passwordController.text.trim();
-                      if (username == '' || password == '') {
-                        displayDialog(
-                            context,
-                            AppLocalizations.of(context)!.fill_all_data,
-                            AppLocalizations.of(context)!
-                                .fill_add_data_description);
-                        LoadingOverlay.of(context).hide();
-                      } else {
-                        var jwt = await AccountRepository()
-                            .attemptLogIn(username, password);
-                        LoadingOverlay.of(context).hide();
-                        if (jwt == "400") {
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Container(
+                    height: 50,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(10)),
+                    child: TextButton(
+                      onPressed: () async {
+                        LoadingOverlay.of(context).show();
+                        var username = usernameController.text.trim();
+                        var password = passwordController.text.trim();
+                        if (username == '' || password == '') {
                           displayDialog(
                               context,
-                              AppLocalizations.of(context)!.error,
+                              AppLocalizations.of(context)!.fill_all_data,
                               AppLocalizations.of(context)!
-                                  .login_pass_mustbe_4long);
-                        } else if (jwt == "401") {
-                          displayDialog(
-                              context,
-                              AppLocalizations.of(context)!.error,
-                              AppLocalizations.of(context)!.login_error);
-                        } else if (jwt == "CONN_ERR") {
-                          displayDialog(
-                              context,
-                              AppLocalizations.of(context)!.connection_error,
-                              AppLocalizations.of(context)!
-                                  .connection_error_description);
-                        } else if (jwt == "SERV_ERR") {
-                          displayDialog(
-                              context,
-                              AppLocalizations.of(context)!.server_error,
-                              AppLocalizations.of(context)!
-                                  .server_error_description);
-                        } else if (jwt != null) {
-                          storage.write(key: "jwt", value: jwt);
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => TabPage()));
+                                  .fill_add_data_description);
+                          LoadingOverlay.of(context).hide();
                         } else {
-                          displayDialog(
-                              context,
-                              AppLocalizations.of(context)!.error,
-                              AppLocalizations.of(context)!.unexpected_error);
+                          var jwt = await AccountRepository()
+                              .attemptLogIn(username, password);
+                          LoadingOverlay.of(context).hide();
+                          if (jwt == "400") {
+                            displayDialog(
+                                context,
+                                AppLocalizations.of(context)!.error,
+                                AppLocalizations.of(context)!
+                                    .login_pass_mustbe_4long);
+                          } else if (jwt == "401") {
+                            displayDialog(
+                                context,
+                                AppLocalizations.of(context)!.error,
+                                AppLocalizations.of(context)!.login_error);
+                          } else if (jwt == "CONN_ERR") {
+                            displayDialog(
+                                context,
+                                AppLocalizations.of(context)!.connection_error,
+                                AppLocalizations.of(context)!
+                                    .connection_error_description);
+                          } else if (jwt == "SERV_ERR") {
+                            displayDialog(
+                                context,
+                                AppLocalizations.of(context)!.server_error,
+                                AppLocalizations.of(context)!
+                                    .server_error_description);
+                          } else if (jwt != null) {
+                            storage.write(key: "jwt", value: jwt);
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => TabPage()));
+                          } else {
+                            displayDialog(
+                                context,
+                                AppLocalizations.of(context)!.error,
+                                AppLocalizations.of(context)!.unexpected_error);
+                          }
                         }
-                      }
-                    },
-                    child: Text(
-                      AppLocalizations.of(context)!.login,
-                      style: TextStyle(color: Colors.white, fontSize: 25),
+                      },
+                      child: Text(
+                        AppLocalizations.of(context)!.login,
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Text(
-                  AppLocalizations.of(context)!.dont_have_an_account,
-                  textAlign: TextAlign.center,
-                  textScaleFactor: 1.4,
-                  overflow: TextOverflow.ellipsis,
+                Padding(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    AppLocalizations.of(context)!.dont_have_an_account,
+                    textAlign: TextAlign.center,
+                    textScaleFactor: 1.4,
+                    overflow: TextOverflow.ellipsis,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.all(10),
-                child: Container(
-                  height: 40,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                      color: Colors.grey[800],
-                      borderRadius: BorderRadius.circular(10)),
-                  child: TextButton(
-                    onPressed: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) =>
-                                  LoadingOverlay(child: signUpView())
-                          )
-                      );
-                    },
-                    child: Text(
-                      AppLocalizations.of(context)!.signup,
-                      style: TextStyle(color: Colors.white, fontSize: 20),
+                Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Container(
+                    height: 40,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Colors.grey[800],
+                        borderRadius: BorderRadius.circular(10)),
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    LoadingOverlay(child: signUpView())
+                            )
+                        );
+                      },
+                      child: Text(
+                        AppLocalizations.of(context)!.signup,
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
                     ),
                   ),
                 ),
-              ),
-            ],
+              ],
           ),
-        )),
+        ),
+            )),
       ),
     );
   }
